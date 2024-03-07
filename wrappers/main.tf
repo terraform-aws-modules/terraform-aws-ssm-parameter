9f7cd842d459f5f751feb088ed1073148b2ea996
Bryant Biggs <bryantbiggs@gmail.com>
2024-03-06 19:06:08 -0500

    fix: Update CI workflow versions to remove deprecated runtime warnings

--- wrappers/main.tf
+++ wrappers/main.tf
@@ -3,17 +3,17 @@ module "wrapper" {
 
   for_each = var.items
 
+  allowed_pattern      = try(each.value.allowed_pattern, var.defaults.allowed_pattern, null)
   create               = try(each.value.create, var.defaults.create, true)
+  data_type            = try(each.value.data_type, var.defaults.data_type, null)
+  description          = try(each.value.description, var.defaults.description, null)
   ignore_value_changes = try(each.value.ignore_value_changes, var.defaults.ignore_value_changes, false)
-  secure_type          = try(each.value.secure_type, var.defaults.secure_type, false)
+  key_id               = try(each.value.key_id, var.defaults.key_id, null)
   name                 = try(each.value.name, var.defaults.name, null)
+  secure_type          = try(each.value.secure_type, var.defaults.secure_type, false)
+  tags                 = try(each.value.tags, var.defaults.tags, {})
+  tier                 = try(each.value.tier, var.defaults.tier, null)
+  type                 = try(each.value.type, var.defaults.type, null)
   value                = try(each.value.value, var.defaults.value, null)
   values               = try(each.value.values, var.defaults.values, [])
-  description          = try(each.value.description, var.defaults.description, null)
-  type                 = try(each.value.type, var.defaults.type, null)
-  tier                 = try(each.value.tier, var.defaults.tier, null)
-  key_id               = try(each.value.key_id, var.defaults.key_id, null)
-  allowed_pattern      = try(each.value.allowed_pattern, var.defaults.allowed_pattern, null)
-  data_type            = try(each.value.data_type, var.defaults.data_type, null)
-  tags                 = try(each.value.tags, var.defaults.tags, {})
 }
